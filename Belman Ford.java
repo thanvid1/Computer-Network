7c1c8d1a02071adf70be8b367236d4f92eee44ea
thanvid1 <tanvviiiiacharya@gmail.com>
2025-09-19 16:14:17 +0530

    Update Belman Ford.java

--- Belman Ford.java
+++ Belman Ford.java
@@ -1,121 +1,89 @@
+// Program 5: Develop a program to find the shortest path between vertices 
+// using the Bellman–Ford algorithm.
 
+package com.smvitm.clab;
+import java.util.Scanner;
 
-package com.smvitm.cnlab;
+public class BellmanFordDemo {
+    static Scanner in = new Scanner(System.in);
 
-import java.util.Scanner,
+    public static void main(String[] args) {
+        int V, E = 1, checkNegative;
+        int w[][] = new int[20][20];
+        int edge[][] = new int[50][2];
 
-public class Bellman Demo Final{
+        // Read number of vertices
+        System.out.print("Enter the number of vertices: ");
+        V = in.nextInt();
 
-static Scanner in = new Scanner(System.in);
+        // Read weight matrix
+        System.out.println("Enter the weight matrix (0 if no edge): ");
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                w[i][j] = in.nextInt();
+                if (w[i][j] != 0) {
+                    edge[E][0] = i;
+                    edge[E][1] = j;
+                    E++;
+                }
+            }
+        }
 
-public static void main(String[] args){
+        // Call Bellman–Ford
+        checkNegative = bellmanFord(w, V, E - 1, edge);
 
-int V, E=1, checkNegative = 0;
+        // Print result
+        if (checkNegative == 1)
+            System.out.println("\nNo negative weight cycle\n");
+        else
+            System.out.println("\nNegative weight cycle exists\n");
+    }
 
-int w[][]=new int[20][20];
+    // Bellman–Ford function
+    public static int bellmanFord(int w[][], int V, int E, int edge[][]) {
+        int u, v, flag = 1, S;
+        int distance[] = new int[20];
+        int parent[] = new int[20];
 
-int edge[][] = new int[50][2];
+        // Initialize distances
+        for (int i = 1; i <= V; i++) {
+            distance[i] = 999;
+            parent[i] = -1;
+        }
 
+        // Read source vertex
+        System.out.print("Enter the source vertex: ");
+        S = in.nextInt();
+        distance[S] = 0;
 
-System.out.println("Enter the no of vertices");
+        // Relax edges V-1 times
+        for (int i = 1; i <= V - 1; i++) {
+            for (int k = 1; k <= E; k++) {
+                u = edge[k][0];
+                v = edge[k][1];
+                if (distance[u] + w[u][v] < distance[v]) {
+                    distance[v] = distance[u] + w[u][v];
+                    parent[v] = u;
+                }
+            }
+        }
 
-V=in.nextInt();
+        // Check for negative weight cycle
+        for (int k = 1; k <= E; k++) {
+            u = edge[k][0];
+            v = edge[k][1];
+            if (distance[u] + w[u][v] < distance[v]) {
+                flag = 0;
+            }
+        }
 
-System.out.println("Enter the weight matrix");
-
-for (int i=1; i < V; i++)
-
-for (int j=1;j<= V; j++)
-
-{
-
-w[i][j]= in.nextInt();
-
-if (w[i][j]!=0)
-
-{
-
-edge[E][0] = 1;
-
-edge[E++][1]=j;
-
-}
-
-chckNegative bellman Ford(w, V, E, edge);
-
-if (chckNegative==1)
-
-System.out.println("\nNo negative weight cycle\n");
-
-else
-
-System.out.println("\nNegative weight cycle exists\n");
-}
-public static int bellman Ford(int w[][], int V, int E, int edge[][]){
-
-int u, v, S, flag=1;
-
-int distance[] = new int[20];
-
-int parent = new int[20];
-
-
-for (int i=1; i<= V; i++){
-distance[ l = 999
-
-parent[i] = -1;
-}
-
-System.out.println("Enter the source vertex");
-
-S=in.nextInt();
-
-distance[S] = 0;
-
-
-for ( int i = 1; i <= V - 1 ;i++)
-
-{
-
-for (int k = 1 k <=E; k++)
-
-{
-
-u=edge[k][0];
-
-v=edge[k][1];
-
-
-if (distance[u] + w[u][v] < distance[v])
-{
-
-distance[v] distance[u] + w[u][v];
-
-parent[v] = u;
-
-}
-
-for (int k = 1 k <-E; k++)
-
-{
-
-u=edge[k][0];
-
-v=edge[k][1];
-
-if (distance[u] + w[u][v] < distance[v])
-
-flag=0;
-} 
-
-
-if (flag==1)
-{
-
-for ( int i = 1; i <= V ;i++){
-
-System.out.println("Vertex "+i+"->cost+distance[i]+" parent="+(parent[i]));
-
-return flag;
-}
+        // Print results if no negative cycle
+        if (flag == 1) {
+            for (int i = 1; i <= V; i++) {
+                System.out.println("Vertex " + i + " -> cost = " + distance[i] +
+                                   " parent = " + parent[i]);
+            }
+        }
+        return flag;
+    }
 }
